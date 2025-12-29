7b6e0890daa68237a8ff6d76eca39b36ef4ac8c5
Gabriele Santi <neonoetica@gmail.com>
2025-12-29 20:49:18 +0000

    Fix version ref

--- platform-catalog/build.gradle.kts
+++ platform-catalog/build.gradle.kts
@@ -27,7 +27,7 @@ publishing {
             from(components["versionCatalog"])
             groupId = "$group"
             artifactId = "spring-boot-ms-version-catalog"
-            version = "1.0.3"
+            version = "1.0.4"
         }
     }
 }
