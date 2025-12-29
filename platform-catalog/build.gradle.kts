174cbc602a701511633251aa8a16cd51f9ca837f
Gabriele Santi <neonoetica@gmail.com>
2025-12-29 05:35:01 +0000

    Split catalog in plat and test

---
+++ platform-catalog/build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    `version-catalog`
+    `maven-publish`
+}
+
+catalog {
+    versionCatalog {
+        from(files("libs.versions.toml"))
+    }
+}
+
+publishing {
+    publishing {
+        repositories {
+            maven {
+                name = "GitHubPackages"
+                url = uri("https://maven.pkg.github.com/Phortran/spring-boot-ms-version-catalog")
+                credentials {
+                    username = System.getenv("GITHUB_ACTOR") ?: findProperty("gpr.user") as String?
+                    password = System.getenv("GITHUB_TOKEN") ?: findProperty("gpr.key") as String?
+                }
+            }
+        }
+    }
+    publications {
+        create<MavenPublication>("catalog") {
+            from(components["versionCatalog"])
+            groupId = "$group"
+            artifactId = "spring-boot-ms-version-catalog"
+            version = "1.0.0"
+        }
+    }
+}
